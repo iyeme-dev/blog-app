5e1d308e1c7985fd57c396197bbfa7383b1f6124
iyeme-dev <iyemeaji@yahoo.com>
2025-10-06 11:19:26 +0100

    Clean server file for Render deployment

--- app.js
+++ app.js
@@ -2,17 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const path = require('path');
-
-// Serve static files from 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve index.html on root
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
+const PORT = process.env.PORT || 3002;
 const PORT = 3002;
 const blogsPath = path.join(__dirname, 'data', 'blogs.json');
 
